9a62fb77642f0043eed6b27cab67e11fb95b34e4
adridevelop <adridevelop11@gmail.com>
2025-11-18 12:55:58 +0100

    Comenzamos con Springboot

---
+++ src/main/java/com/ipartek/spring/elpisito/apirest/models/entities/Usuario.java
@@ -0,0 +1,53 @@
+package com.ipartek.spring.elpisito.apirest.models.entities;
+
+import java.io.Serial;
+import java.io.Serializable;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+@Entity
+@Table(name="usuarios")
+public class Usuario implements Serializable{
+
+	@Serial
+	private static final long serialVersionUID = 6309547869169816180L;
+
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY) // IDENTITY es un incremental para MySQL
+	@Column
+	private Long id;
+	
+	@Column(unique = true)
+	private String nombre;
+	
+	@Column
+	private String password;
+	
+	@Column
+	private String passwordOpen;
+	
+	@Column(unique = true)
+	private String email;
+	
+	@Column
+	private String rol = "ROLE_USER";
+	
+	@Column
+	private Integer activo = 1;
+
+}
